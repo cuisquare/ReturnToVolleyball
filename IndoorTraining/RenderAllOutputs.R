23fd43de1fd5e2a15446c19c725cab9dc818cf6e
cuisquare <cuisquare@gmail.com>
2021-10-08 11:10:50 +0100

    docx output added and kable bug sorted hopefully

---
+++ IndoorTraining/RenderAllOutputs.R
@@ -0,0 +1,13 @@
+#render all outputs
+library(rmarkdown)
+library(bookdown)
+
+render(input = "./IndoorTraining/LSKVC_IndoorTraining_RiskAssessment.Rmd",output_format = "pdf_document"
+       )
+render(input = "./IndoorTraining/LSKVC_IndoorTraining_PlayersRules.Rmd"#,output_format = "all"
+       )
+render(input = "./IndoorTraining/LSKVC_IndoorTraining_ClubRequirements.Rmd"#,output_format = "all"
+       )
+
+#?render
+
